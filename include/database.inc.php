34ae646165950054bba0887b26294ab623815f6b
quentinTresdoi <tresdoi14@gmail.com>
2022-10-20 14:04:30 +0200

    inscription

--- include/database.inc.php
+++ include/database.inc.php
@@ -1,4 +1,7 @@
 <?php
+
+session_start();
+
 class BDD
 {
     private $dbname;
@@ -26,5 +29,77 @@ class BDD
             die();
         }
         return $this->bdd;
+    }
+
+    public function getLogin($myTable, $myEmail, $myPwd)
+    {
+        $sql = 'SELECT * FROM ' . $myTable . ' WHERE pasword = ' . $myPwd . ' AND email = "' . $myEmail . '"';
+
+        $req = $this->bdd->prepare($sql);
+        $req->execute();
+
+        $result = $req->fetchAll();
+        $count = $req->rowCount();
+
+        date_default_timezone_set('Europe/Paris');
+
+        if ($count > 0) {
+            foreach ($result as $row) {
+
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['pseudo'] = $row['pseudo'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['date'] = date('Y-m-d H:i:s');
+
+                header("Refresh:0; url=myaccount.php");
+            }
+        } else {
+            echo '<p style="color: #ff0000; display: flex; justify-content: center; margin-bottom: 30px;">Mot de passe ou email invalide</p>';
+        }
+    }
+
+    public function addUser($myTable, $_email, $_passwordbdd, $_pseudo)
+    {
+        $sql = 'SELECT * FROM ' . $myTable . ' WHERE pseudo = "' . $_pseudo . '" OR email = "' . $_email . '"';
+        $req = $this->bdd->prepare($sql);
+        $req->execute();
+
+        $result = $req->fetchAll();
+        $count = $req->rowCount();
+
+        if ($count > 0) {
+            echo '<p style="color: #ff0000; display: flex; justify-content: center; margin-bottom: 30px;">pseudo ou l\'email est déjà utilisé</p>';
+        } else {
+            $sql2 = 'INSERT INTO ' . $myTable . ' (email, pasword, pseudo, date_inscription) VALUES ("' . $_email . '", "' . $_passwordbdd . '", "' . $_pseudo . '", NOW())';
+            $req2 = $this->bdd->prepare($sql2);
+            $req2->execute();
+
+            header("Refresh:0; url=login.php");
+        }
+    }
+
+    public function selectMessage($myTable)
+    {
+        $sql = 'SELECT message, pseudo, date, CASE WHEN user_id = ' . $_SESSION['user_id'] . ' THEN TRUE ELSE FALSE END AS SENDER FROM ' . $myTable . ' INNER JOIN users ON messages.user_id = users.id WHERE date > DATE_SUB(NOW(), INTERVAL 1 DAY) ORDER BY date ASC';
+        $req = $this->bdd->prepare($sql);
+        $req->execute();
+
+        $result = $req->fetchAll();
+
+        foreach ($result as $row) {
+            if ($row['SENDER'] == true) {
+                echo '<div class="send"><p>Moi</p><div class="bull"><p>' . $row['message'] . '</p></div><p>' . $row['date'] . '</p></div>';
+            } else {
+                echo '<div class="reçu"><div class="text"><p>' . $row['pseudo'] . '</p></div><div><img src="./assets/images/chat/png-clipart-computer-icons-person-name-miscellaneous-computer-wallpaper.png" alt="img"><div class="bull"><p>' . $row['message'] . '</p></div></div><div class="text"><p>' . $row['date'] . '</p></div></div>';
+            }
+        }
+    }
+
+    public function insertMessage($myTable, $myMessage)
+    {
+        $sql = 'INSERT INTO ' . $myTable . ' (game_id, user_id, message, date) VALUES (1, 4, "' . $myMessage . '", NOW())';
+
+        $req = $this->bdd->prepare($sql);
+        $req->execute();
     }
 }
--- register.php
+++ register.php
@@ -11,30 +11,52 @@
 
     <title>Inscription</title>
 </head>
+<?php
+require_once './include/database.inc.php';
+$bdd = new BDD('localhost', 'puissance4', 'root', '', 'bdd');
+
+$bdd->getmybdd();
+?>
 
 <body>
 
     <?php require "./view/header.inc.php"; ?>
 
-    <div class="bannière"><h1>Inscription</h1></div>
+
+    <div class="bannière">
+        <h1>Inscription</h1>
+    </div>
     <div class="login-box">
         <h2>Inscription</h2>
-        <form>
+        <form action="./register.php" method="POST">
             <div class="user-box">
-                <input type="text" placeholder="Email">
+                <input type="email" placeholder="Email" name="email">
             </div>
 
             <div class="user-box">
-                <input type="password" placeholder="Pseudo">
+                <input type="text" placeholder="Pseudo" name="pseudo">
             </div>
             <div class="user-box">
-                <input type="password" placeholder="Mot de passe">
+                <input type="password" placeholder="Mot de passe" name="passewordbdd">
             </div>
             <div class="user-box">
-                <input type="password" placeholder="Confirmer le mot de passe">
+                <input type="password" placeholder="Confirmer le mot de passe" name="pwdb">
             </div>
+            <?php
+            if (isset($_POST['submit'])) {
+                if (strlen($_POST['passewordbdd']) < 8) {
+                    echo '<p style="color: #ff0000; display: flex; justify-content: center; margin-bottom: 30px;">Le mot de passe doit faire plus de 8 character</p>';
+                } elseif ($_POST['passewordbdd'] != $_POST['pwdb']) {
+                    echo '<p style="color: #ff0000; display: flex; justify-content: center; margin-bottom: 30px;">Les mots passe doivent être identiques</p>';
+                } elseif (strlen($_POST['pseudo']) < 4) {
+                    echo '<p style="color: #ff0000; display: flex; justify-content: center; margin-bottom: 30px;">Le pseudo est trop court</p>';
+                } else {
+                    $bdd->addUser('users', $_POST['email'], $_POST['passewordbdd'], $_POST['pseudo']);
+                }
+            }
+            ?>
             <div class="button-form">
-                <a id="créer-le-compte" href="#">Finalisez l'inscription</a>
+                <input type="submit" id="créer-le-compte" value="Finalisez l'inscription" name='submit'>
 
                 <div id="login">
                     Vous avez déjà un compte ?
@@ -42,6 +64,7 @@
                 </div>
             </div>
         </form>
+
     </div>
 
     <?php require "./view/footer.inc.php"; ?>
